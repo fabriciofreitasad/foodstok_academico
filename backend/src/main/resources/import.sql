8b6584e6c7751bce71d06cec2bba5253fb343e4c
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2024-09-15 15:43:48 -0300

    seeds..

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -9,7 +9,7 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (4, 1);
 
